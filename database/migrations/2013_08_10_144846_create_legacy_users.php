8f59e2cae6aa70d42661dd466cc017f627cd1908
Richard Muvirimi <richard@tyganeutronics.com>
2024-05-17 15:18:51 +0200

    Set user name default value to empty string

--- database/migrations/2013_08_10_144846_create_legacy_users.php
+++ database/migrations/2013_08_10_144846_create_legacy_users.php
@@ -15,7 +15,7 @@ return new class extends Migration
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
                 $table->string('email')->unique();
-                $table->string('name');
+                $table->string('name')->default('');
                 $table->string('password');
                 $table->timestamps();
                 $table->softDeletes();
